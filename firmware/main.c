3288734eeaf733fbf6efda563df0608a2048ae6b
francosauvisky <francosauvisky@gmail.com>
2020-07-19 14:59:03 -0300

    initial firmware files

---
+++ firmware/main.c
@@ -0,0 +1,45 @@
+#include <avr/io.h>
+#include <avr/eeprom.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+#include <avr/cpufunc.h>
+#include <avr/sleep.h>
+#include <util/atomic.h>
+
+FUSES =
+{
+    .low = (LFUSE_DEFAULT),
+    .high = (HFUSE_DEFAULT),
+};
+
+#define F_CPU 8000000UL
+#include <util/delay.h>
+
+// definitions/macros:
+#define SET_FLAG(x) (g_flags|=(x))
+#define CLEAR_FLAG(x) (g_flags&=~(x))
+#define TOGGLE_FLAG(x) (g_flags^=(x))
+#define FLAG_IS_SET(x) (g_flags&(x))
+#define FLAG_IS_CLEAR(x) (~g_flags&(x))
+#define MAXOF(x,y) (((x)>(y))?(x):(y))
+#define MINOF(x,y) (((x)<(y))?(x):(y))
+
+// function prototypes:
+
+// global variables:
+
+// eeprom variables:
+
+void
+main (void)
+{
+  // uint8_t initial_s = MCUSR;
+  // MCUSR = 0;
+  wdt_enable(WDTO_250MS);
+  wdt_reset();
+
+  while(1) // main loop
+  {
+    wdt_reset();
+  }
+}
