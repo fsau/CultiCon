2c70462118475e377fea3d5fc5e871e2765b1bd4
francosauvisky <francosauvisky@gmail.com>
2020-07-21 18:44:30 -0300

    split main

---
+++ firmware/interrupts.c
@@ -0,0 +1,22 @@
+ISR(INT0_vect)
+{
+
+}
+
+// dimmer sync pulse
+ISR(TIMER1_CAPT_vect)
+{
+
+}
+
+// dimmer routines:
+ISR(TIMER1_COMPA_vect)
+{
+
+}
+
+// general timing routines
+ISR(TIMER1_COMPB_vect)
+{
+
+}
--- firmware/main.c
+++ firmware/main.c
@@ -1,43 +1,6 @@
-#include <avr/io.h>
-#include <avr/eeprom.h>
-#include <avr/interrupt.h>
-#include <avr/wdt.h>
-#include <avr/cpufunc.h>
-#include <avr/sleep.h>
-#include <util/atomic.h>
-
 #define F_CPU 8000000UL
-#include <util/delay.h>
-
 #define BAUD 9600
-#include <util/setbaud.h>
-
-FUSES =
-{
-    .low = (LFUSE_DEFAULT),
-    .high = (HFUSE_DEFAULT),
-};
-
-// definitions/macros:
-#define SET_FLAG(x) (g_flags|=(x))
-#define CLEAR_FLAG(x) (g_flags&=~(x))
-#define TOGGLE_FLAG(x) (g_flags^=(x))
-#define FLAG_IS_SET(x) (g_flags&(x))
-#define FLAG_IS_CLEAR(x) (~g_flags&(x))
-#define MAXOF(x,y) (((x)>(y))?(x):(y))
-#define MINOF(x,y) (((x)<(y))?(x):(y))
-
-// structs:
-typedef struct _clock_time
-{
-  uint8_t hour;
-  uint8_t minute;
-  uint8_t second;
-} clock_time;
-
-// function prototypes:
-void get_i2c_time(void);
-void set_i2c_time(void);
+#include "pinout_def.h"
 
 // global variables:
 clock_time g_ctime;
@@ -45,6 +8,12 @@ clock_time g_ctime;
 // eeprom variables:
 clock_time EEMEM turn_on, turn_off;
 uint8_t EEMEM tpow_on, tpow_off;
+
+// functions:
+#include "gfunctions.c"
+
+// interrupts:
+#include "interrupts.c"
 
 // main:
 void
@@ -62,36 +31,3 @@ main (void)
     wdt_reset();
   }
 }
-
-// interrupts:
-ISR(INT0_vect)
-{
-
-}
-
-ISR(INT1_vect)
-{
-
-}
-
-// functions:
-
-void
-get_i2c_time(void)
-{
-
-}
-
-void
-set_i2c_time(void)
-{
-
-}
-
-/*
-sketches:
-
-dimmers:
-
-
-*/
