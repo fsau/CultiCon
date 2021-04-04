3c001874f028e6dfdcd7a9392c8d53a47f4c6f60
francosauvisky <francosauvisky@gmail.com>
2021-04-04 14:20:11 -0300

    cleanup & display node initial wip

--- firmware/lcdmenus.c
+++ firmware/lcdmenus.c
@@ -6,36 +6,10 @@ LCDContext lcd_status;
 
 void lcd_update()
 {
-  switch(lcd_status.disp)
-  {
-    case lcd_main:
-      break;
-    case lcd_menu:
-      break;
-    case lcd_edit:
-      break;
-    case lcd_debug:
-      break;
-    default:
-    case lcd_off:
-      break;
-  }
+
 }
 
 void lcd_input(LCDInput)
 {
-  switch(lcd_status.disp)
-  {
-    case lcd_main:
-      break;
-    case lcd_menu:
-      break;
-    case lcd_edit:
-      break;
-    case lcd_debug:
-      break;
-    default:
-    case lcd_off:
-      break;
-  }
+
 }
--- firmware/lcdmenus.h
+++ firmware/lcdmenus.h
@@ -3,30 +3,16 @@
 #define LINE_SIZE 16
 #define LINE_N 2
 
-typedef enum _lcd_mode_e
-{
-  lcd_off=0, lcd_main, lcd_menu, lcd_edit, lcd_debug,
-} LCDMode;
-
-typedef enum _lcd_argtype_e
-{
-  arg_null=0, arg_temp, arg_time, arg_num, arg_bit,
-} LCDArgType;
-
 typedef enum _lcd_input_e
 {
-  lcd_input_none=0, lcdinput_back=1, lcd_input_incr=2, lcd_input_decr=4,
+  lcd_input_none=0, lcd_input_home, lcd_input_ok, lcd_input_incr, lcd_input_decr,
 } LCDInput;
 
 typedef struct _lcd_context_s
 {
-  LCDDisp disp;
   PGM_P *string;
-  LCDArgType type;
-  void *arg;
-} LCDContext;
-
-
+  const uint8_t *arg;
+} LCDDisp;
 
 // Public functions:
 
